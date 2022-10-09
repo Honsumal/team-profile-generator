ada2368cf50ccb82d70674683075b6144f304fef
honsumal <14leealastair@gmail.com>
2022-10-08 21:33:47 -0400

    added model for team member cards

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const questions = [
             if (nInput){
                 return true
             } else {
-                console.log('Please enter the name of your team member.')
+                console.log(' Please enter the name of your team member.')
                 return false
             }
         }
@@ -42,7 +42,7 @@ const questions = [
             if (iInput) {
                 return true
             } else {
-                console.log('Please enter the id of your team member.')
+                console.log(' Please enter the id of your team member.')
                 return false
             }
         }
@@ -55,7 +55,7 @@ const questions = [
             if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(eInput)){ //obtained from regexr.com email address validation
                 return true
             } else {
-                console.log('Please enter a valid email address.')
+                console.log(' Please enter a valid email address.')
                 return false
             }
         }
@@ -65,11 +65,25 @@ const questions = [
         message: 'Please enter your engineer\'s github username: ',
         name: 'github',
         when: (input) => input.role === 'Engineer',
-        validate: (nameInput) => {
-            if (nameInput) {
+        validate: (gInput) => {
+            if (gInput) {
                 return true
             } else {
-                console.log('Please enter your engineer\'s github username.')
+                console.log(' Please enter your engineer\'s github username.')
+                return false
+            }
+        }
+    },
+    {
+        type: 'input',
+        message: 'Please enter your intern\'s school: ',
+        name: 'school',
+        when: (input) => input.role === 'Intern',
+        validate: (sInput) => {
+            if (sInput) {
+                return true
+            } else {
+                console.log(' Please enter your engineer\'s github username.')
                 return false
             }
         }
@@ -78,8 +92,61 @@ const questions = [
 
 ]
 
-function init () {
+function getInput () {
+    return inq.prompt(questions)
+        .catch(err => {
+            console.log(err)
+        })
+}
 
+function cardBuilder (input) {
+    switch (input.role) {
+        case "Employee" :
+            return `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${input.name}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Employee</h6>
+          <p class="card-text">ID: ${input.id}</p>
+          <p class="card-text">Email: ${input.email}</p>
+        </div>
+      </div>`;
+        case "Engineer" :
+            return `<div class = "card" style = "width: 18rem;">
+            <div class = "card-body">
+              <h5 class = "card-title">${input.name}</h5>
+              <h6 class = "card-subtitle mb-2 text-muted">Engineer</h6>
+              <p class = "card-text">ID: ${input.id}</p>
+              <p class = "card-text">Email: ${input.email}</p>
+              <p class = "card-text">Github: <a href = "https://github.com/${input.github}">https://github.com/${input.github}</a></p>
+            </div>
+          </div>`
+        case "Intern" :
+            return `<div class = "card" style = "width: 18rem;">
+            <div class = "card-body">
+              <h5 class = "card-title">${input.name}</h5>
+              <h6 class = "card-subtitle mb-2 text-muted">Employee</h6>
+              <p class = "card-text">ID: ${input.id}</p>
+              <p class = "card-text">Email: ${input.email}</p>
+              <p class = "card-text">School Name: ${input.school}</p>
+            </div>
+          </div>`;
+        case "Manager" :
+            return `<div class = "card" style = "width: 18rem;">
+            <div class = "card-body">
+              <h5 class = "card-title">${input.name}</h5>
+              <h6 class = "card-subtitle mb-2 text-muted">Employee</h6>
+              <p class = "card-text">ID: ${input.id}</p>
+              <p class = "card-text">Email: ${input.email}</p>
+              <p class = "card-text">Office Number: ${input.school}</p>
+            </div>
+          </div>`
+    }
+}
+
+async function init () {
+    const input = await getInput();
+    fs.writeFile('./dist/holmgang.txt', JSON.stringify(input), (err) =>
+    err ? console.error(err) : console.log('Input logged!'))
 }
 
 init()
